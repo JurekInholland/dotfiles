64e7f5f74d76238551f5e696028cdd468d36f4e4
JurekInholland <jurekbaumann@me.com>
2019-01-10 23:55:40 +0100

    reload shell

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -11,8 +11,8 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim zshrc oh-my-zsh private scrotwm.conf Xresources"    # list of files/folders to symlink in homedir
-
+# files="bashrc vimrc vim zshrc oh-my-zsh private scrotwm.conf Xresources"    # list of files/folders to symlink in homedir
+files="bash_aliases"
 ##########
 
 # create dotfiles_old in homedir
--- script.sh
+++ script.sh
@@ -1,2 +1,6 @@
+apt-get install 
+
 sh makesymlinks.sh
 echo "script executed"
+
+source ~/.bash_aliases
