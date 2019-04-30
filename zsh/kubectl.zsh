4aecc23aef5002396060fcae03a835c5c18969d5
JurekInholland <jurekbaumann@me.com>
2019-04-30 04:43:27 +0200

    add kubectl .zsh file

---
+++ zsh/kubectl.zsh
@@ -0,0 +1,21 @@
+alias kc="kubectl"
+
+alias kcaliases="cat ~/.k8s-aliases"
+
+alias kca="kubectl apply -f ."
+alias kcd="kubectl delete -f ."
+
+alias kce="kubectl edit -it"
+
+alias kcan="kubectl --all-namespaces"
+
+alias kcl="kubectl logs"
+
+alias kex="kubectl exec -it"
+
+alias kdp="kubectl describe pods"
+
+alias kpf="kubectl port-forward"
+
+alias kga='kubectl get all'
+alias kgaa='kubectl get all --all-namespaces'
