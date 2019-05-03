735d4bc192fee5f5daa05322ed6df3426e784594
Jurekinholland <jurekbaumann@me.com>
2019-05-03 05:55:12 +0200

    Add mount alias

---
+++ zsh/aliases.zsh
@@ -0,0 +1,4 @@
+alias update="apt update && sudo apt upgrade"
+alias inst="apt install -y"
+alias uninst="apt remove -y"
+alias mnto="mount -t overlay -o lowerdir=/mnt/GDA,upperdir=/mnt/md0/local-storage,workdir=/mnt/md0/work overlay /mnt/overlay"
--- zsh/bash_aliases.zsh
+++
@@ -1,3 +0,0 @@
-alias update="apt update && sudo apt upgrade"
-alias inst="apt install -y"
-alias uninst="apt remove -y"
